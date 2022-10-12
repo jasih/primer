9124166ae6ec575641285348f7a105ed8586d951
Janet Sihapanya <janetsihapanya@gmail.com>
2022-10-11 19:26:01 -0500

    Completed Primer Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export function calculateTotal (items, tax) {
-  // TODO
+export function calculateTotal(items, tax) {
+  let total = 0;
+  for (let item of items) {
+    const { price, taxable } = item;
+    if (taxable) {
+      total += price + price * Math.abs(tax);
+    } else {
+      total += price;
+    }
+  }
+  return total;
 }
